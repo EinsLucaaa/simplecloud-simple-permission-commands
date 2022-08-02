7e9dd5f7e64f8402021e46cffc521f7f208bee39
EinsLucaaa <74465789+EinsLucaaa@users.noreply.github.com>
2022-08-02 09:02:16 +0200

    fix maybe

--- src/main/kotlin/me/luca/permission/command/RankCommand.kt
+++ src/main/kotlin/me/luca/permission/command/RankCommand.kt
@@ -2,15 +2,18 @@ package me.luca.permission.command
 
 import cloud.commandframework.Command
 import cloud.commandframework.arguments.standard.IntegerArgument
+import cloud.commandframework.arguments.standard.LongArgument
 import cloud.commandframework.arguments.standard.StringArgument
 import cloud.commandframework.bukkit.BukkitCommandManager
 import eu.thesimplecloud.api.CloudAPI
 import eu.thesimplecloud.module.permission.PermissionPool
 import eu.thesimplecloud.module.permission.player.PlayerPermissionGroupInfo
+import me.luca.permission.SimplePermissionPlugin
 import me.luca.permission.util.UUIDFetcher
 import org.bukkit.command.CommandSender
 import org.bukkit.command.ConsoleCommandSender
 import java.util.concurrent.TimeUnit
+import java.util.logging.Level
 
 /**
  * Created by EinsLuca
@@ -27,20 +30,22 @@ class RankCommand(
         commandManager.command(commandBuilder
             .argument(StringArgument.newBuilder("player"))
             .argument(StringArgument.newBuilder("group"))
-            .argument(IntegerArgument.newBuilder("timeout"))
+            .argument(LongArgument.newBuilder("timeout"))
             .handler {
                 val sender = it.sender
 
                 if (sender is ConsoleCommandSender) {
                     val player = it.get<String>("player")
                     val group = it.get<String>("player")
-                    val timeOut = it.get<Int>("timeout")
+                    val timeOut = it.get<Long>("timeout")
 
                     val permissionPlayer = PermissionPool.instance.getPermissionPlayerManager().getCachedPermissionPlayer(UUIDFetcher.getUUID(player))
 
-                    val timeOutLong = System.currentTimeMillis() + TimeUnit.DAYS.toMillis(timeOut.toLong())
+                    val timeOutLong = System.currentTimeMillis() + TimeUnit.DAYS.toMillis(timeOut)
                     permissionPlayer!!.addPermissionGroup(PlayerPermissionGroupInfo(group, timeOutLong))
                     permissionPlayer.update()
+
+                    SimplePermissionPlugin.instance.logger.log(Level.INFO, "Giving $group to ${UUIDFetcher.getUUID(player)}/$player for $timeOut days!")
 
                 }
 
